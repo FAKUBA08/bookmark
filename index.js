54111209e6851c521df0fcdf68e59288a5788917
FAKUBA08 <badmusfaruq01@gmail.com>
2024-06-06 08:41:03 +0100

    all

--- index.js
+++ index.js
@@ -3,85 +3,85 @@ const underlineColor = document.querySelector('.underlineColor');
 const seenBtn=document.querySelector('.section3-inner ul li')
 
 lastItems.forEach(li => {
-  li.addEventListener('click', function() {
-    lastItems.forEach(items => {
-      items.classList.remove('clicked');
-    });
-    this.classList.add('clicked');
-  });
+li.addEventListener('click', function() {
+lastItems.forEach(items => {
+items.classList.remove('clicked');
+});
+this.classList.add('clicked');
+});
 });
 document.addEventListener("DOMContentLoaded", function() {
-  const bookmarkingDiv = document.querySelector('.bookmarking');
-  const speedsearchingDiv = document.querySelector('.speedsearching');
-  const easysharingDiv = document.querySelector('.easysharing');
+const bookmarkingDiv = document.querySelector('.bookmarking');
+const speedsearchingDiv = document.querySelector('.speedsearching');
+const easysharingDiv = document.querySelector('.easysharing');
 
-  const bookmarkButton = document.querySelector('.book');
-  const speedsearchingButton = document.querySelector('.speed');
-  const easysharingButton = document.querySelector('.easy');
+const bookmarkButton = document.querySelector('.book');
+const speedsearchingButton = document.querySelector('.speed');
+const easysharingButton = document.querySelector('.easy');
 
-  bookmarkButton.addEventListener('click', function() {
-      bookmarkingDiv.removeAttribute('hidden');
-      speedsearchingDiv.setAttribute('hidden', true);
-      easysharingDiv.setAttribute('hidden', true);
-  });
+bookmarkButton.addEventListener('click', function() {
+bookmarkingDiv.removeAttribute('hidden');
+speedsearchingDiv.setAttribute('hidden', true);
+easysharingDiv.setAttribute('hidden', true);
+});
 
-  speedsearchingButton.addEventListener('click', function() {
-    // if (seenBtn) {
-    //   underlineColor.style.marginTop="57px"
-    //   console.log("seen");
-    //   alert('dcjhdbc')
-    
-    // }
-      bookmarkingDiv.setAttribute('hidden', true);
-      speedsearchingDiv.removeAttribute('hidden');
-      easysharingDiv.setAttribute('hidden', true);
+speedsearchingButton.addEventListener('click', function() {
+// if (seenBtn) {
+//   underlineColor.style.marginTop="57px"
+//   console.log("seen");
+//   alert('dcjhdbc')
+
+// }
+bookmarkingDiv.setAttribute('hidden', true);
+speedsearchingDiv.removeAttribute('hidden');
+easysharingDiv.setAttribute('hidden', true);
 
 
-  });
+});
 
-  easysharingButton.addEventListener('click', function() {
-      bookmarkingDiv.setAttribute('hidden', true);
-      speedsearchingDiv.setAttribute('hidden', true);
-      easysharingDiv.removeAttribute('hidden');
-      // if (seenBtn.style.display==='block') {
-    
-      //   underlineColor.style.marginTop="115px"
-      // }
-  });
+easysharingButton.addEventListener('click', function() {
+bookmarkingDiv.setAttribute('hidden', true);
+speedsearchingDiv.setAttribute('hidden', true);
+easysharingDiv.removeAttribute('hidden');
+// if (seenBtn.style.display==='block') {
+
+//   underlineColor.style.marginTop="115px"
+// }
+});
 });
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  const lastItem = document.querySelectorAll('.section3-inner ul li');
+const lastItem = document.querySelectorAll('.section3-inner ul li');
 
-  function activateUnderline() {
-    const firstLi = lastItem[0]; 
-    const containerRect = firstLi.parentElement.getBoundingClientRect();
-    const rect = firstLi.getBoundingClientRect();
-    const leftPosition = rect.left - containerRect.left;
+function activateUnderline() {
+const firstLi = lastItem[0]; 
+const containerRect = firstLi.parentElement.getBoundingClientRect();
+const rect = firstLi.getBoundingClientRect();
+const leftPosition = rect.left - containerRect.left;
 
-    underlineColor.style.width = rect.width + 'px';
-    underlineColor.style.transform = `translateX(${leftPosition}px)`;
-  }
+underlineColor.style.width = rect.width + 'px';
+underlineColor.style.transform = `translateX(${leftPosition}px)`;
+}
 
-  
-  activateUnderline();
 
-  lastItem.forEach(li => {
-    li.addEventListener('click', function() {
-      lastItem.forEach(item => {
-        item.classList.remove('clicked');
-      });
-      this.classList.add('clicked');
+activateUnderline();
 
-      const containerRect = this.parentElement.getBoundingClientRect();
-      const rect = this.getBoundingClientRect();
-      const leftPosition = rect.left - containerRect.left;
+lastItem.forEach(li => {
+li.addEventListener('click', function() {
+lastItem.forEach(item => {
+item.classList.remove('clicked');
+});
+this.classList.add('clicked');
 
-      underlineColor.style.width = rect.width + 'px';
-      underlineColor.style.transform = `translateX(${leftPosition}px)`;
-    });
-  });
+const containerRect = this.parentElement.getBoundingClientRect();
+const rect = this.getBoundingClientRect();
+const leftPosition = rect.left - containerRect.left;
+
+underlineColor.style.width = rect.width + 'px';
+underlineColor.style.transform = `translateX(${leftPosition}px)`;
+});
+});
 });
 
 
@@ -92,41 +92,48 @@ let imgBoxes = document.querySelectorAll('.img-box img');
 let accordion = document.getElementsByClassName('accordion');
 
 for (let index = 0; index < accordion.length; index++) {
-  accordion[index].addEventListener('click', function() {
-    let panel = this.nextElementSibling;
+accordion[index].addEventListener('click', function() {
+let panel = this.nextElementSibling;
 
-    imgBoxes[index].classList.toggle('rotate180');
+imgBoxes[index].classList.toggle('rotate180');
 
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
-  });
+if (panel.style.display === 'block') {
+panel.style.display = 'none';
+} else {
+panel.style.display = 'block';
+}
+});
 }
 const submit=document.getElementById('submit')
 const inputBox=document.querySelector('.input-box')
 const con=document.querySelector(".con")
 let invalid=document.querySelector('.invalid')
- const container6=document.querySelector('.container6')
+const container6=document.querySelector('.container6')
 submit.addEventListener('click',function(){
-    if (inputBox.value.includes('@gmail.com')) { 
-    window.location.href='index.html'
+if (inputBox.value.includes('@gmail.com' || '@yahoo.com' || '@outlook.com')) { 
+window.location.href='index.html'
 
-      }else{
-        document.querySelector('.invalid').style.display='block'
-        document.querySelector('.con').style.display='block'
-        invalid.innerHTML='<h4 class="error"><img src="images/icon-error.svg" alt=""></h4>'
-    
-        inputBox.value=''
-         container6.style.marginBottom='-68px'
-        const res=setTimeout(() => {
-        document.querySelector('.invalid').style.display='none'
-        document.querySelector('.con').style.display='none'
-        container6.style.marginBottom='-15px'
-        clearTimeout(res)
-        },2000);
-      }
+}
+else if (inputBox.value.includes( '@yahoo.com' || '@outlook.com')) {
+window.location.href='index.html'
+}
+else if (inputBox.value.includes('@outlook.com')) {
+window.location.href='index.html'
+}
+else{
+document.querySelector('.invalid').style.display='block'
+document.querySelector('.con').style.display='block'
+invalid.innerHTML='<h4 class="error"><img src="images/icon-error.svg" alt=""></h4>'
+
+inputBox.value=''
+container6.style.marginBottom='-68px'
+const res=setTimeout(() => {
+document.querySelector('.invalid').style.display='none'
+document.querySelector('.con').style.display='none'
+container6.style.marginBottom='-15px'
+clearTimeout(res)
+},2000);
+}
 
 })
 const hambouger=document.querySelector('.hambouger')
@@ -134,12 +141,14 @@ let hamHome=document.querySelector('.hambouger-menu')
 let hamAppearance=document.getElementById('ham-appearance')
 let cancelHome=document.querySelector('.cancel-ham')
 hambouger.addEventListener('click',function(){
- hamHome.style.display='block'
-//  hamAppearance.style.display='none'
- 
+hamHome.style.display='block'
+if (hamHome) {
+hamHome.style.position='fixed'
+}
+
 })
 cancelHome.addEventListener('click',function(){
- hamHome.style.display='none'
+hamHome.style.display='none'
 })
 
 
@@ -148,6 +157,6 @@ cancelHome.addEventListener('click',function(){
 
 
 
-  
-  
+
+
 
